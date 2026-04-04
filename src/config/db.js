1fc147623c642808e27cea2700823c2652aa6971
Your Name <you@example.com>
2026-04-04 09:52:58 +0530

    chore: Finalize documentation and revert DB driver for Sequelize compatibility

--- src/config/db.js
+++ src/config/db.js
@@ -2,9 +2,10 @@ const { Sequelize } = require('sequelize');
 const path = require('path');
 
 const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: path.join(__dirname, '../../database.sqlite'),
-  logging: false,
+    dialect: 'sqlite',
+    dialectModule: require('better-sqlite3'),
+    storage: path.join(__dirname, '../../database.sqlite'),
+    logging: false,
 });
 
 module.exports = sequelize;
